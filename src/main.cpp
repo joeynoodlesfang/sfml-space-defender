825072b9955246f29c443de46802f20fbaff316b
JF <zhoufang29@gmail.com>
2025-03-20 01:37:45 -0700

    imported example project file

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    window.setFramerateLimit(144);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
